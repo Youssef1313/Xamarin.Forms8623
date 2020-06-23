58d157d749be17037e1d3f34a8788bbb7888bf6f
Youssef Victor <youssefvictor00@gmail.com>
2020-06-23 08:27:19 +0200

    Slight improvements

--- App1/App1/MainPage.xaml.cs
+++ App1/App1/MainPage.xaml.cs
@@ -111,11 +111,24 @@ namespace App1
                     dest.BackgroundColor = Color.Cyan;
                     _colored.Add(dest);
                 }
+                
 
             }
             else
             {
-                _board.MakeMove(new Move(_activeSelection, chessSquare.Square, _board.WhoseTurn(), PawnPromotion.Queen), true);
+                PawnPromotion? promotion = null;
+                if (_board[_activeSelection] is Pawn && (chessSquare.Square.Rank == Rank.First || chessSquare.Square.Rank == Rank.Eighth))
+                {
+                    string strProm;
+                    do
+                    {
+                        strProm = await DisplayActionSheet("Promote to what ?", null, null, "Queen", "Rook", "Knight", "Bishop");
+                    } while (strProm == null);
+                    Enum.TryParse(strProm, out PawnPromotion temp_promotion);
+                    promotion = temp_promotion;
+                }
+                var move = new Move(_activeSelection, chessSquare.Square, _board.WhoseTurn(), promotion);
+                _board.MakeMove(move, isMoveValidated: true);
 
                 ChessSquare src = this.FindByName<ChessSquare>(_activeSelection.ToString());
                 
@@ -123,25 +136,34 @@ namespace App1
                 Piece p1 = _board[chessSquare.Square];
                 Piece p2 = _board[_activeSelection];
 
-                if (p1 == null)
+                if (p1 is King && move.GetAbsDeltaX() == 2 ||
+                    p1 is Pawn && move.GetAbsDeltaX() == 1) // Draw complete board only on Special Moves (Castle, Enpassant).
                 {
-                    chessSquare.Source = "";
+                    DrawBoard();
                 }
-                else
+                else // On regular moves, draw particular squares only. (For performance reasons)
                 {
-                    chessSquare.Source = $"{p1.Owner}{p1.GetType().Name}";
-                    chessSquare.Rotation = (p1.Owner == Player.White) ? 0 : 180;
+                    if (p1 == null)
+                    {
+                        chessSquare.Source = "";
+                    }
+                    else
+                    {
+                        chessSquare.Source = $"{p1.Owner}{p1.GetType().Name}";
+                        chessSquare.Rotation = (p1.Owner == Player.White) ? 0 : 180;
+                    }
+
+                    if (p2 == null)
+                    {
+                        src.Source = "";
+                    }
+                    else
+                    {
+                        src.Source = $"{p2.Owner}{p2.GetType().Name}";
+                        src.Rotation = (p2.Owner == Player.White) ? 0 : 180;
+                    }
                 }
 
-                if (p2 == null)
-                {
-                    src.Source = "";
-                }
-                else
-                {
-                    src.Source = $"{p2.Owner}{p2.GetType().Name}";
-                    src.Rotation = (p2.Owner == Player.White) ? 0 : 180;
-                }
                 ClearColored();
 
                 GameState state = _board.GameState;
---
+++ App1/App1/Models/ChessSquare.cs
@@ -0,0 +1,19 @@
+﻿using ChessLibrary.SquareData;
+using Xamarin.Forms;
+
+namespace App1.Models
+{
+    public class ChessSquare : Image
+    {
+        public Square Square { get; set; }
+
+        //public static readonly BindableProperty SquareProperty =
+        //    BindableProperty.Create(nameof(Square), typeof(Square), typeof(ChessSquare), null);
+
+        //public Square Square
+        //{
+        //    get { return Square.Parse((string)GetValue(SquareProperty)); }
+        //    set { SetValue(SquareProperty, value); }
+        //}
+    }
+}
